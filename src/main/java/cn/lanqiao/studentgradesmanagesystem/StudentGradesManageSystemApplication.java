b6e21e005690168c46fceac985a1e3accc274945
DataClass-WJC <3293180029@qq.com>
2024-12-18 20:38:00 +0800

    前端页面

---
+++ src/main/java/cn/lanqiao/studentgradesmanagesystem/StudentGradesManageSystemApplication.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.studentgradesmanagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGradesManageSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentGradesManageSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lanqiao/studentgradesmanagesystem/StudentGradesManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.studentgradesmanagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGradesManageSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
