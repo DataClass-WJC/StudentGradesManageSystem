2fffc6950098023ce4ec6ecc7fede3644c6817f2
111 <3496405075@qq.com>
2024-12-31 10:48:41 +0800

    登录的

---
+++ src/main/java/cn/lanqiao/studentgradesmanagesystem/service/impl/teacherServiceImpl.java
@@ -0,0 +1,27 @@
+package cn.lanqiao.studentgradesmanagesystem.service.impl;
+
+import cn.lanqiao.studentgradesmanagesystem.mapper.teacherMapper;
+import cn.lanqiao.studentgradesmanagesystem.pojo.Teacher;
+import cn.lanqiao.studentgradesmanagesystem.service.teacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class teacherServiceImpl implements teacherService {
+    //依赖注入
+    @Autowired
+    private teacherMapper teacherMapper;
+
+
+    @Override
+    public Teacher login(Teacher teacher) {
+        Teacher result = teacherMapper.login(teacher);
+        if(result != null){
+            //登录成功
+            return result;
+        }else {
+            //登录失败
+            return null;
+        }
+    }
+}
